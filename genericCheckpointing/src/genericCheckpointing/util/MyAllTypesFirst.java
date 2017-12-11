3cf3e423c0e15d8bfb76cf7ef1a685f22a05edc9
HarshadLoya <harshadloya@gmail.com>
2017-12-11 04:16:16 -0500

    Overridden toString, hashcode and equals method.

--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
@@ -72,4 +72,53 @@ public class MyAllTypesFirst extends SerializableObject
 	public void setmyBool(boolean myBoolIn) {
 		myBool = myBoolIn;
 	}
+
+	@Override
+	public String toString()
+	{
+		return "MyAllTypesFirst: myInt=" + myInt + ", myOtherInt=" + myOtherInt + ", myLong=" + myLong
+				+ ", myOtherLong=" + myOtherLong + ", myString=" + myString + ", myBool=" + myBool;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (myBool ? 1231 : 1237);
+		result = prime * result + myInt;
+		result = prime * result + (int) (myLong ^ (myLong >>> 32));
+		result = prime * result + myOtherInt;
+		result = prime * result + (int) (myOtherLong ^ (myOtherLong >>> 32));
+		result = prime * result + ((myString == null) ? 0 : myString.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MyAllTypesFirst other = (MyAllTypesFirst) obj;
+		if (myBool != other.myBool)
+			return false;
+		if (myInt != other.myInt)
+			return false;
+		if (myLong != other.myLong)
+			return false;
+		if (myOtherInt != other.myOtherInt)
+			return false;
+		if (myOtherLong != other.myOtherLong)
+			return false;
+		if (myString == null) {
+			if (other.myString != null)
+				return false;
+		} else if (!myString.equals(other.myString))
+			return false;
+		return true;
+	}
 }
